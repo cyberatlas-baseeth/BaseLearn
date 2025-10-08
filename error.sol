178085b17a989a1b9d4aac70cce26b0fafaf3542
Cyber <tahabiyikli61@gmail.com>
2025-10-08 23:04:57 +0300

    Update error.sol

--- error.sol
+++ error.sol
@@ -3,44 +3,33 @@
 pragma solidity ^0.8.17;
 
 contract ErrorTriageExercise {
-    /**
-     * @dev Finds the difference between each uint with its neighbor (a to b, b to c, etc.)
-     * and returns a uint array with the absolute integer difference of each pairing.
-     * 
-     * @param _a The first unsigned integer.
-     * @param _b The second unsigned integer.
-     * @param _c The third unsigned integer.
-     * @param _d The fourth unsigned integer.
-     * 
-     * @return results An array containing the absolute differences between each pair of integers.
-     */
+
     function diffWithNeighbor(
         uint _a,
         uint _b,
         uint _c,
         uint _d
     ) public pure returns (uint[] memory) {
-        // Initialize an array to store the differences
+        // Farkları saklamak için bir dizi başlat
         uint[] memory results = new uint[](3);
 
-        // Calculate the absolute difference between each pair of integers and store it in the results array
+        // Her bir tamsayı çifti arasındaki mutlak farkı hesapla ve sonucu results dizisinde sakla
         results[0] = _a > _b ? _a - _b : _b - _a;
         results[1] = _b > _c ? _b - _c : _c - _b;
         results[2] = _c > _d ? _c - _d : _d - _c;
-
-        // Return the array of differences
+        
+        
         return results;
     }
 
-    /**
-     * @dev Changes the base by the value of the modifier. Base is always >= 1000. Modifiers can be
-     * between positive and negative 100.
-     * 
-     * @param _base The base value to be modified.
-     * @param _modifier The value by which the base should be modified.
-     * 
-     * @return returnValue The modified value of the base.
-     */
+
+ //@dev Temel değeri, değiştirici değerine göre değiştirir. Temel değer her zaman 1000 veya daha büyük olmalıdır. 
+ //Değiştiriciler, pozitif veya negatif 100 arasında olabilir.
+ //@param _base Değiştirilecek temel değer.
+ //@param _modifier Temel değeri değiştirecek olan değer.
+ 
+ //@return returnValue Temel değerin değiştirilmiş hali.
+
     function applyModifier(
         uint _base,
         int _modifier
