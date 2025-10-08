e3ad467d4b38d650bcf0ec645b0a2fc842731200
Cyber <tahabiyikli61@gmail.com>
2025-10-08 22:47:32 +0300

    Update Structs.sol

--- Structs.sol
+++ Structs.sol
@@ -47,3 +47,44 @@ contract GarageManager {
     }
 
     /**
+     * @dev Belirtilen kullanıcının arabalarını getirir
+     * @param _user Arabaları görüntülenecek kullanıcının adresi
+     * @return Belirtilen kullanıcının `Car` struct dizisini döner
+     */
+    function getUserCars(address _user) external view returns (Car[] memory) {
+        // İlgili kullanıcının garajındaki arabaları döndürür
+        return garages[_user];
+    }
+
+    /**
+     * @dev Çağıran kullanıcının garajındaki belirli bir arabayı günceller
+     * @param _index Güncellenecek arabanın garajdaki indeksi
+     * @param _make Yeni marka bilgisi
+     * @param _model Yeni model bilgisi
+     * @param _color Yeni renk bilgisi
+     * @param _numberOfDoors Yeni kapı sayısı
+     */
+    function updateCar(
+        uint256 _index,
+        string memory _make,
+        string memory _model,
+        string memory _color,
+        uint _numberOfDoors
+    ) external {
+        // Eğer girilen indeks geçersizse özel hata fırlatır
+        if (_index >= garages[msg.sender].length) {
+            revert BadCarIndex({index: _index});
+        }
+
+        // İlgili arabanın bilgilerini yeni değerlerle günceller
+        garages[msg.sender][_index] = Car(_make, _model, _color, _numberOfDoors);
+    }
+
+    /**
+     * @dev Çağıran kullanıcının garajındaki tüm arabaları siler
+     */
+    function resetMyGarage() external {
+        // Kullanıcının garajındaki tüm arabaları siler (diziyi sıfırlar)
+        delete garages[msg.sender];
+    }
+}
